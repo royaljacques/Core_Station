59acb85e9cd5edef3fbf996dcba8b2bf11bc3770
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2020-10-19 14:19:34 +0200

    Ajout du Join

--- src/Core/Event/Join.php
+++ src/Core/Event/Join.php
@@ -2,11 +2,25 @@
 
 use Core\Main;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 
 class Join implements Listener
 {
+	/**
+	 * @var Main
+	 */
+	private $plugin;
+
 	public function __construct(Main $plugin)
 	{
-		
+		$this->plugin = $plugin;
+	}
+	public function OnJoin(PlayerJoinEvent $e)
+	{
+		$player = $e->getPlayer();
+		if (!$player->hasPlayedBefore())
+		{
+			$player->sendMessage("Bienvenue dans le serveur");
+		}
 	}
 }
